635d0404b673ae3708c10a98514f72d477ec5370
RogerioAP <rogerioalmpe@hotmail.com>
2012-06-15 19:21:33 -0300

    Adicao do menu drop down, inclusao do banner, inclusao de borda degrade

--- home.php
+++ home.php
@@ -4,11 +4,32 @@
     <head>
             <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
             <link href="jeito.css" rel="StyleSheet" type="text/css">
-            <title>Sua Moda</title>
+			<link href="css/default.css" rel="stylesheet" type="text/css" />
+	
+			<!--2 arquivos javascript para o slideshow-->
+			<script src="javascript/jquery-1.2.6.pack.js" type="text/javascript"></script>
+			<script src="javascript/jquery.flow.1.1.min.js" type="text/javascript"></script>
+            
+			<title>Sua Moda</title>
+			
+			<!--Javascript que define as imagens do slideshow-->
+			<script type="text/javascript">
+			$(function() {
+				$("div#controller").jFlow({
+					slides: "#slides",
+					width: "800px",
+					height: "313px"
+				});
+			});
+			</script>
     </head>
     <body class="bodyW">
-        <div><!--Principal-->
-            <div class="cabecalho"><!--Cabeçalho-->
+		
+		<!--Div que recebe tudo e que tem a borda degradê-->
+		<div class="div-borda">
+		
+			<!--Cabeçalho-->
+            <div class="cabecalho">
 				<a href="home.php" class="image_title"><div class="image_title"></div></a>
 				<div class="pes"><!--Espaco "Pessoal"-->
 					<?php
@@ -46,57 +67,32 @@
 					?>
 				</div>
             </div>
-            <?php include_once 'designer.inc'; menu();?>  <!--***MENU***-->
-            <div class="content"><!--Conteúdo-->
-				<div><!--COLOCAR CONDICOES PHP PARA PRODUTOS CUSTOMIZADOS PARA CLIENTE-->
-					<div>Construindo...
-					</div>
-					<div><!--DESTAQUES-->
-						Destaques<hr/>
-						<!--<img src="picture/virtual.jpg">-->
-						<!--KKKKKKKKKK-->
-						<script src='javascript/jquery.js' type='text/javascript'></script>
-						<script src='javascript/s3Slider.js' type='text/javascript'></script>
-						<script type="text/javascript">
-						$(document).ready(function()
-						{
-							$('#slider').s3Slider({
-							timeOut: 3000 /*Representa em milesimos de segundo o tempo de troca de imagem (neste caso temos 3 segundos)*/
-						});
-						});
-						</script>
-						<div id="slider">
-							<ul id="sliderContent">
-								<li class="sliderImage">
-									<a href="#"><img src="produtos/blusas.jpg" border="0"/>
-									<span class="top">Blusas</span></a>
-								</li>
-								<li class="sliderImage">
-									<a href="#"><img src="produtos/adesivo_celular.jpg" border="0"/>
-									<span class="top">Adesivo p/ Celular</span></a>
-								</li>
-								<li class="sliderImage">
-									<a href="#"><img src="produtos/sapatos_bolsa.jpg" border="0"/>
-									<span class="top">Sapato e Bolsa</span></a>
-								</li>
-								<li class="sliderImage">
-									<a href="#"><img src="produtos/sapatos.jpg" border="0"/>
-									<span class="top">Sapatinhos</span></a>
-								</li>
-								<li class="sliderImage">
-									<a href="#"><img src="produtos/adesivo_notebook.jpg" border="0"/>
-									<span class="top">Adesivo p/ Notebook</span></a>
-								</li>
-								<div class="clear sliderImage"></div>	
-							</ul>
-						</div>
-					</div>
-					<!--KKKKKKKKKK-->
-					<!--<img src="produtos/1.jpg">
-					<img src="produtos/2.jpg">
-					<img src="produtos/3.jpg">
-					<img src="produtos/4.jpg">-->
+			
+			<!--***MENU***-->
+            <?php include_once 'designer.inc'; menu();?>
+			
+			<!--Div do slideshow-->
+			<div id="wrap" style="margin-top:-45px;padding:10px 0px 0px 300px;"><!--style="padding:10px 0px 0px 300px;">-->
+				<div id="controller" class="hidden">
+					<span class="jFlowControl">No 1</span>
+					<span class="jFlowControl">No 2</span>
+					<span class="jFlowControl">No 3</span>
 				</div>
+				
+				<div id="prevNext">
+					<img src="picture/prev.png" alt="Previous Tab" class="jFlowPrev" />
+					<img src="picture/next.png" alt="Next Tab" class="jFlowNext" />
+				</div>
+				
+				<div id="slides">
+					<div><a href="#"><img src="picture/1.jpg" alt="photo" /></a><p>This is photo number one. Neato!</p></div>
+					<div><a href="#"><img src="picture/2.jpg" alt="photo" /></a><p>This is photo number two. Neato!</p></div>
+					<div><a href="#"><img src="picture/3.jpg" alt="photo" /></a><p>This is photo number three. Neato!</p></div>
+				</div>
+			</div>
+
+			<!--Conteúdo-->
+            <div class="content">				
 				<div style="width:100%;">
 					Novidades meninas, olhem!<hr>
 					<table border="1">
@@ -116,6 +112,8 @@
 					</table>
 				</div>
             </div>
+			
+			<!--Rodapé-->
 			<?php include_once 'designer.inc'; rodape(); ?>
         </div>
     </body>
---
+++ venda.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+            <link href="jeito.css" rel="StyleSheet" type="text/css">
+			
+            <title>Sua Moda</title>
+    </head>
+    <body class="bodyW">
+        <div><!--Principal-->
+            <div class="cabecalho"><!--Cabeçalho-->
+				<?php
+					session_start();
+					include_once "connect.php";
+					if(isset($_SESSION['logado']) && $user["tipo"]=='a')
+					{
+						echo "<div class='image_title'></div>";
+					}
+					else
+					{
+						echo "<a href='home.php' class='image_title'><div class='image_title'></div></a>";
+					}
+				?>
+				
+				<div class="pes"><!--Espaco "Pessoal"-->
+					<?php
+						$caminho_imagem = "fotos/803fbd58f1ed97adb518c3b2f6cc6d7a.png"; /*imagem que vai ser utilizada como padrão caso o cliente não escolha nenhuma*/
+						//Iniciando a sessão
+						if(isset($_SESSION['logado']))
+						{
+							$sql = "SELECT * FROM sis_login WHERE idusuario = ".$_SESSION['id_user'];
+							
+							$rs = mysql_query($sql);
+							if(mysql_num_rows($rs))
+							{
+								global $user;
+								$user = mysql_fetch_array($rs);
+								$nome = $user["nome"]; /*nome completo*/
+								$foto = $user["foto"];
+								if($user["tipo"]=='a'){$linha=$nome;} else {$linha="<a href='user_d_pessoais.php' style='color:black;'>$nome</a>";}//desativar link no nome
+								echo "<table border='0' style='float:right'>
+										<tr>
+											<td colspan='2'><img src='$foto' width='55px' height='60px'></td>
+										</tr>
+										<tr>
+											<td>$linha</td>
+											<td><a href='logout.php' style='color:red;'>Sair</a></td>
+										</tr>
+									  </table>";
+							}
+						}
+						else
+						{
+							echo "<div><a href='cadastrar.php'>Cadastrar</a></div><br><br>";
+							echo "<div><a href='login.php'>Login</a></div>";
+						}
+					?>
+				</div>
+            </div>
+            <?php
+				if($user["tipo"]=='a')
+				{
+					include_once 'designer.inc'; menu_admin();
+				}
+				else
+				{
+					include_once 'designer.inc'; menu();
+				}
+			?>  <!--***MENU***-->
+            <div class="content"><!--Conteúdo-->
+				<?php //////////////************CADASTRANDO INCLUSIVE IMAGEM NO BD*********////////////
+					// Conexão com o banco de dados
+					//$conn = @mysql_connect("localhost", "root", '') or die ("Problemas na conexão com o banco de dados.");
+					//$db = @mysql_select_db("site", $conn) or die ("Problemas na conexão com a tabela de dados.");
+					 
+					// Se o usuário clicou no botão cadastrar efetua as ações
+					if (isset($_POST['cadastrar']))
+					{
+						// Recupera os dados dos campos
+						$nome = $_POST['nome'];
+						$sobrenome = $_POST['sobrenome'];
+						$cpf = $_POST['cpf'];
+						$telefone = $_POST['telefone'];
+						$endereco = $_POST['endereco'];
+						$numero = $_POST['numero'];
+						$bairro = $_POST['bairro'];
+						$cidade = $_POST['cidade'];
+						$email = $_POST['email'];
+						$password = $_POST['password'];
+						$password2 = $_POST['password2'];
+						if($user["tipo"]=='a'){$usuario = 'a';}else{$usuario = 'u';}//para cadastro usuário ou admin
+						
+						// Faz a verificação da extensão do arquivo
+						// Array com as extensões permitidas
+
+						/*$extensao = strtolower(end(explode('.', $_FILES['foto']['name'])));						
+						if (array_search($extensao, $_UP['extensoes']) === false) {
+						echo "Por favor, envie arquivos com as seguintes extensões: jpg, png ou gif";						
+						}else{echo "certo!";}*/
+						
+						// testa pra ver se os dados foram preenchidos
+						if(!empty($nome) && !empty($sobrenome) && !empty($cpf) && !empty($telefone) && !empty($endereco) && !empty($numero) && !empty($bairro)
+							&& !empty($cidade) && !empty($email) && !empty($password) && !empty($password2) && $password==$password2) //(!empty($foto["name"]))
+						{
+							//testa pra saber se a imagem foi carregada
+							if($_FILES['foto']['error']==0)
+							{
+								$foto = $_FILES['foto'];
+								// Tamanho máximo do arquivo em bytes
+								$tamanho = 100000;
+								
+								// Verifica se o tamanho da imagem é maior que o tamanho permitido
+								if($foto["size"] > $tamanho)
+								{
+									echo "<center>A imagem deve ter no máximo ".$tamanho." bytes</center><br>";
+								}
+								else
+								{
+									$_UP['extensoes'] = array('jpg', 'jpeg', 'bmp', 'png', 'gif');
+									//if($ext[1] != "jpg" && $ext[1] != "bmp" && $ext[1] != "png" && $ext[1] != "gif" && $ext[1] != "jpg" && $ext[1] != "jpeg")
+									$extensao = strtolower(end(explode('.', $_FILES['foto']['name'])));						
+									if (array_search($extensao, $_UP['extensoes']) === false)
+									{
+									   echo "<center>Isso não é uma imagem!</center><br>";
+									}
+									else
+									{
+										// Pega as dimensões da imagem
+										$dimensoes = getimagesize($foto["tmp_name"]);
+										
+										preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
+										//echo "<br>".$ext[1];
+										// Gera um nome único para a imagem
+										$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+							 
+										// Caminho de onde ficará a imagem
+										$caminho_imagem = "";
+										$caminho_imagem = "fotos/" . $nome_imagem;
+							 
+										// Faz o upload da imagem para seu respectivo caminho
+										move_uploaded_file($foto["tmp_name"], $caminho_imagem);
+									}
+									//echo "INSERT INTO usuarios VALUES (null, \"$nome\", \"$email\", \"$nome_imagem\");";
+									// Insere os dados no banco
+							////////$sql = mysql_query("INSERT INTO sis_login VALUES (null, \"$nome\", \"$email\", \"$password\", \"$caminho_imagem\");");//\"rog\", \"ema\", \"nom\")");//(null, '".$nome."', '".$email."', '".$nome_imagem."')");
+								}
+							}
+							include "classe.php";
+							$obj = new Classe; /* usando a função INSERIR do arquivo classe.php */
+							$obj->inserir($nome, $sobrenome, $cpf, $telefone, $endereco, $numero, $bairro, $cidade, $email, $password, $caminho_imagem, $usuario);
+							echo "<br><center style='color:green;'>Cadastrado com sucesso!</center><br>";
+						}
+						else
+						{//echo "<br><center style='color:red;'>Construindo! Aguarde...</center>";
+							echo "<br><center style='color:red;'>Os campos com * não podem ficar em branco!</center><br>";
+						}
+					}
+					$est_ses = session_id();
+					if(empty($est_ses))
+					{
+						session_start();
+					}
+					include_once("connect.php");
+					
+					$texto = "CADASTRAMENTO";
+					$tipo = "";
+					if(isset($_SESSION['logado'])){$tipo = $user["tipo"];if($tipo=='a'){$texto='ADICIONAR&nbsp&nbsp&nbsp&nbspADMINISTRADOR';}}/*pega o tipo de usuário logado*/
+					if(isset($_SESSION['logado']) && $tipo == 'u') /*se existir usuário logado dá ERRO (afinal como logar em outra conta já estando logado)*/
+					{
+						echo "<br><center>Existe um usuário ativo no momento, <a href='logout.php'>clique aqui</a> para sair e entrar em outra conta.</center><br>";
+					}
+					else
+					{
+						echo "<form method='post' action='cadastrar.php' enctype='multipart/form-data'>
+							<br><center>Compra</center>
+							<table border='0'>
+								<tr>
+									<td colspan='2'><u>Dados do Usuário</u></td>
+								</tr>
+								<tr>
+									<td>Nome</td>
+									<td><input type='text' id='txt' name='nome' maxlength='10' placeholder='Digite o 1° nome'></td><!--Limmite de 10 dígitos neste campo-->
+								</tr>
+								<tr>
+									<td>CPF</td>
+									<td><input type='text' id='txt' name='cpf' placeholder='Digite o CPF'></td>
+								</tr>
+								<tr>
+									<td colspan='2'><br><u>Dados de Endereço</u></td>
+								</tr>
+								<tr>
+									<td>* Endereço</td>
+									<td><input type='text' id='txt' name='endereco' placeholder='Digite o endereço'></td>
+								</tr>
+								<tr>
+									<td>* Cidade</td>
+									<td><input type='text' id='txt' name='cidade' placeholder='Digite a cidade'></td>
+								</tr>
+								<tr>
+									<td colspan='2'><br><u>Dados de Identificação</u></td>
+								</tr>
+								<tr>
+									<td>* Senha</td>
+									<td><input type='password' id='txt' name='password' placeholder='Digite a senha'></td>
+								</tr>
+								<tr>
+									<td>Valor Total</td>
+									<td><input type='text' id='txt' name='password'></td>
+								</tr>
+								<tr>
+									<td colspan='2'><br><button name='cadastrar'>Cadastrar</button></td>
+								</tr>
+							</table>
+						</form>";
+					}
+				?>
+            </div>
+			<?php include_once 'designer.inc'; rodape(); ?>
+        </div>
+    </body>
+</html>
